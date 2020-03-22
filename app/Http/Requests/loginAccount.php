8e0c0d058d078f5d6a839f66e31e51053f11e376
DESKTOP-0S1IBUO\Admin <wbxkmk2@Gmail.com>
2020-03-22 17:29:48 +0900

    リクエストフォーム追加

--- app/Http/Requests/loginAccount.php
+++ app/Http/Requests/loginAccount.php
@@ -24,7 +24,21 @@ class loginAccount extends FormRequest
     public function rules()
     {
         return [
-            
+            'name' => 'required|between:1,32',
+            'password'=>'required|between:1,32|same:password2',
         ];
+    }
+
+    public function messages()
+    {
+        return[     
+            'name.required' => 'ユーザー名をしてください。',
+            'name.between' => 'ユーザー名は1~32文字で入力して下さい。',
+            'email.email'=> 'メールアドレスの形式が正しくありません。',
+            'email.unique'=> 'すでに同じメールアドレスが登録されています。',
+            'password1.required'=>'パスワード入力してください。',
+            'password1.between'=>'パスワードは1~32文字で入力して下さい。',
+            'password1.same'=>'パスワードが一致しません。',
+           ];   
     }
 }
--- app/Http/Requests/postProfile.php
+++ app/Http/Requests/postProfile.php
@@ -3,20 +3,12 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use  Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
 
 class postProfile extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
-    public function authorize()
-    {
-        return false;
-    }
-
-    /**
+ /**
      * Get the validation rules that apply to the request.
      *
      * @return array
@@ -24,7 +16,40 @@ class postProfile extends FormRequest
     public function rules()
     {
         return [
-            //
+            'profile_name'=> 'required|between:1,32', 
+            'profile_comment'=> 'required|between:1,255', 
         ];
+    }
+
+    public function messages()
+    {
+        return[     
+            'profile_name.required' => 'ユーザー名を入力してください。',
+            'profile_name.between' => 'ユーザー名は1~32文字で入力して下さい。',
+            'profile_comment.required'=> 'コメントが入力されていません。',
+            'profile_comment.between' => 'コメントは1~255文字で入力して下さい。',
+           ];       
+    }
+
+         /**
+     * @Override
+     * 勝手にリダイレクトさせない
+     * @param  \Illuminate\Contracts\Validation\Validator  $validator
+     */
+    protected function failedValidation(Validator $validator) {
+        $res = response()->json([
+            'status' => 400,
+            'errors' => $validator->errors(),
+        ], 400);
+        throw new HttpResponseException($res);
+    }
+
+    /**
+     * バリデータを取得する
+     * @return  \Illuminate\Contracts\Validation\Validator  $validator
+     */
+    public function getValidator()
+    {
+        return $this->validator;
     }
 }
