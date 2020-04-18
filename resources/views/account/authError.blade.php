949c2972f82a40acf86d2b0b56c87977278b5b15
DESKTOP-0S1IBUO\Admin <wbxkmk2@Gmail.com>
2020-04-18 23:20:44 +0900

    登録エラー画面表示変更

--- resources/views/account/authError.blade.php
+++ resources/views/account/authError.blade.php
@@ -7,14 +7,14 @@
 @section('contents')
 <div class="container mt-5">
     <div class="row">
-        <div class="col-10 offset-1 col-md-6 offset-md-3  p-5 mt-5 border border-info"> 
-        <p>{{$message}}</p>
+        <div class="col-10 offset-1 col-md-6 offset-md-3 p-5 mt-5 bg-light border border-info"> 
+            <p>{{$message}}</p>
 
-        <div class="col-12">
+            <div class="col-12">
                 <a href="{{url('/login')}}" class="btn col text-white bg-base mt-3 p-2">
                 <span>ログイン</span></a>
             </div>
-    
+        
             <div class="col-12">
                 <a href="{{url('/authSingup')}}" class="btn col border-primary text-base mt-3 p-2">
                 <span>新規登録</span></a>
@@ -22,5 +22,5 @@
         </div>   
     </div>
 </div>
-  @endsection
+@endsection
   
