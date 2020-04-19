36733006a7cc5296364f47928752b3987fba6c21
DESKTOP-0S1IBUO\Admin <wbxkmk2@Gmail.com>
2020-04-19 17:31:49 +0900

    コメントボタン修正

--- resources/views/component/detaile.blade.php
+++ resources/views/component/detaile.blade.php
@@ -32,7 +32,7 @@
       @endif 
     </div>
 
-      <div class="col-12 text-right">
+      <div class="col-8 offset-4 text-right">
         <button type="button" class="btn func_button" data-toggle="modal" data-target="#modal">
             <i class="fas fa-reply"></i>
             コメント
