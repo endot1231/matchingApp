162070e0087d0b147f9c4ca30365ca467a6693d2
DESKTOP-0S1IBUO\Admin <wbxkmk2@Gmail.com>
2020-04-19 17:33:34 +0900

    ヘッダー戻りボタン追加

--- resources/views/home/profile.blade.php
+++ resources/views/home/profile.blade.php
@@ -4,6 +4,20 @@
 
 @section('contents')
 <div class="container mt-5 pl-0 pr-0">
+
+  <header class="bg-base fixed-top">
+    <div class="container">
+      <div class="row text-white">
+        <div class="col-1 offset-md-3">
+          <i class="fas fa-arrow-left fa-2x mt-1" onclick="history.back()"></i>
+        </div>      
+        <div class="col-8 offset-1 offset-sm-0 col-md-5 text-left p-0">
+          <h2> プロフィール</h2>
+        </div>
+      </div>
+    </div>      
+  </header>
+    
   <div class="row">
     <nav class="col-12 col-lg-3 order-lg-0 order-1">
       @include('component.sidebar')
