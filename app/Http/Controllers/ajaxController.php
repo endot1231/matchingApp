95da2b80bb2d976b3f82c1376f46dbd0300b4023
DESKTOP-0S1IBUO\Admin <wbxkmk2@Gmail.com>
2020-04-26 18:44:44 +0900

    画像傾き修正

--- app/Http/Controllers/ajaxController.php
+++ app/Http/Controllers/ajaxController.php
@@ -117,7 +117,7 @@ class ajaxController extends Controller
             $imageName = str_shuffle(time().$file->getClientOriginalName()). '.' .$file->getClientOriginalExtension();
             $user->icon = $user_id."/".$imageName;
             \Storage::makeDirectory($user_id);
-            \Storage::putFileAs($user_id,$file, $imageName,'public');
+            \Storage::putFileAs($user_id,$image, $imageName,'public');
         }
 
         $user->save();
