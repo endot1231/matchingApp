e06b3e0b4a2a762b731dc386cef4aeae63e6189e
DESKTOP-0S1IBUO\Admin <wbxkmk2@Gmail.com>
2020-05-10 23:07:50 +0900

    コントローラー一部リポジトリを使用するように変更

--- app/Http/Controllers/homeController.php
+++ app/Http/Controllers/homeController.php
@@ -6,20 +6,29 @@ use Illuminate\Http\Request;
 use App\commentsModel;
 use App\postsModel;
 use App\users;
+use App\interfaces\PostsTableInterFace;
 
 class homeController extends Controller
 {
+    protected $postsRepository;
+
+    public function __construct(PostsTableInterFace $postsRepository)
+    {
+       $this->postsRepository = $postsRepository;
+    }
+
     public function index(Request $request)
     {
+        // コンテンツ取得
+        $contents = $this->postsRepository->getPosts(20);
+
         if (!$request->session()->exists('user_id')) 
         {
-            $contents = postsModel::orderBy('post_id','desc')->take(20)->get();
             return view('home.index',['contents'=>$contents] );
         }
 
         // セッションIDの再発行
         $request->session()->regenerate();
-        $contents = postsModel::orderBy('post_id','desc')->take(20)->get();
         return view('home.index',['contents'=>$contents]);
     }
 
