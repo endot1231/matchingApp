ad850eb39046bc46100595554068ede53eb37874
DESKTOP-0S1IBUO\Admin <wbxkmk2@Gmail.com>
2020-05-14 21:24:08 +0900

    PostsRepository修正

--- app/Repository/PostsRepository.php
+++ app/Repository/PostsRepository.php
@@ -14,21 +14,30 @@ class postsRepository implements PostsTableInterFace
         $this->posts = $posts;
     }
 
-    public function registPosts()
+    public function regist(int $post_id,int $user_id,string $title,string $comment)
     {
+        $postCount = $this->posts->all()->Count();   
+        $data['post_id'] = $postCount; 
+        $data['user_id'] = $user_id;
+        $data['title'] = $title;
+        $data['comment'] = $comment;
 
+        $this->posts->create($data);
     }
 
-    public function getPosts(int $count)
+    public function getPosts(int $count,int $skip = 0)
     {
-        $contents =$this->posts->orderBy('post_id','desc')->take(20)->get();
-        return $contents;
+        return $this->posts->orderBy('post_id','desc')->skip($skip)->take($count)->get();
     }
 
-    public function getPostsById(int $post_id)
+    public function getPostsByPost_Id(int $post_id)
     {
-        $content = $this->posts->where('post_id','=',$post_id)->first();
-        return $content;
+        return $this->posts->where('post_id','=',$post_id)->first();      
+    }
+    
+    public function getPostsByUser_Id(int $user_id)
+    {
+        return $this->posts->where('user_id','=',$user_id)->orderBy('post_id', 'desc')->get();
     }
 
     public function getPostsALL()
