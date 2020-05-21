f6ff8c0da9a0b317daca0ae6653ea6e54f2563f9
DESKTOP-0S1IBUO\Admin <wbxkmk2@Gmail.com>
2020-05-21 21:38:58 +0900

    Postsインターフェース登録処理変更

--- app/Interfaces/PostsTableInterFace.php
+++ app/Interfaces/PostsTableInterFace.php
@@ -4,7 +4,7 @@ namespace App\interfaces;
 
 interface PostsTableInterFace
 {
-    public function regist(int $post_id,int $user_id,string $title,string $comment,File $request_File);
+    public function regist(int $post_id,int $user_id,string $title,string $comment);
     public function getPosts(int $count,int $skip);
     public function getPostsByPost_Id(int $post_id);
     public function getPostsByUser_Id(int $user_id);
