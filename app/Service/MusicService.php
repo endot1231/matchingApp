87334c7f727b2fe5a998812fd409bfdcf00dd03e
DESKTOP-0S1IBUO\Admin <wbxkmk2@Gmail.com>
2020-05-24 20:49:02 +0900

    Musicサービス修正

--- app/Service/MusicService.php
+++ app/Service/MusicService.php
@@ -2,33 +2,34 @@
 
 namespace App\Service;
 use App\interfaces\MusicTableInterFace;
-use App\Repository\postsRepository;
-use Faker\Provider\File;
-use League\Flysystem\Exception;
+use App\interfaces\PostsTableInterFace;
 
-class UserService
+use League\Flysystem\Exception;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\DB;
+
+class MusicService
 {
   private $posts_repo;
   private $music_repo;
   
-  public function __construct(postsRepository $posts_repo,MusicTableInterFace $music_repo)
+  public function __construct(PostsTableInterFace $posts_repo,MusicTableInterFace $music_repo)
   {
     $this->posts_repo = $posts_repo;
     $this->music_repo = $music_repo;
   }
   
-  public function regist(int $user_id,string $music_title,string $music_comment,File $request_File)
+  public function regist(int $user_id,string $music_title,string $music_comment,UploadedFile $request_File):bool
   {
+    $result = false;
     DB::beginTransaction();
 
     try
     {
       // カウント取得
-      $postId = $this->posts->all()->Count();
-      $musicId =  $this->music->all()->Count(); 
-
-      $fileBasePath =env('STORAGE_ENDPOINT');
-
+      $postId = $this->posts_repo->allCount();
+      $musicId =  $this->music_repo->allCount(); 
+      
       // ファイル名作成
       $imageName = str_shuffle(time().$request_File->getClientOriginalName()). '.' .$request_File->getClientOriginalExtension();
       $filePath = $user_id.'/'.$imageName;
@@ -39,11 +40,13 @@ class UserService
       \Storage::makeDirectory($user_id);
       \Storage::putFileAs($user_id,$request_File, $imageName,'public');
       DB::commit();
+      
+      $result = true;
     }
     catch(Exception $e)
     {
       DB::rollbadk();
     }
+    return $result;
   }
-
 }
