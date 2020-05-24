4c2b3af751149f33d6e15e42a8fff44ddf70c26a
DESKTOP-0S1IBUO\Admin <wbxkmk2@Gmail.com>
2020-05-24 20:52:38 +0900

    CommentsRepository登録・件数取得追加

--- app/Repository/CommentsRepository.php
+++ app/Repository/CommentsRepository.php
@@ -14,9 +14,24 @@ class CommentsRepository implements CommentsTableInterFace
         $this->comments = $comments;
     }
 
+    public function regist(int $comment_id,int $post_id,int $user_id,string $contents)
+    { 
+        $data['comment_id'] = $comment_id; 
+        $data['post_id'] = $post_id; 
+        $data['user_id'] = $user_id;    
+        $data['contents'] = $contents;
+
+        $this->comments->create($data);
+    }
+
     public function getComments(int $post_id)
     {
         $comments = $this->comments->where('post_id','=',$post_id)->get();
         return $comments;
+    }
+
+    public function allCount():int
+    {
+        return $this->comments::all()->Count();
     }
 }
