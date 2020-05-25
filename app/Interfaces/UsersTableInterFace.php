a560593b26fb91efac5d33fbaade55444b18d02f
DESKTOP-0S1IBUO\Admin <wbxkmk2@Gmail.com>
2020-05-25 21:25:34 +0900

    Userインターフェース修正

--- app/Interfaces/UsersTableInterFace.php
+++ app/Interfaces/UsersTableInterFace.php
@@ -4,6 +4,7 @@ namespace App\interfaces;
 
 interface UsersTableInterFace
 {
-    public function registPosts();
+    public function regist();
+    public function update(int $user_id,string $user_name,string $cmment,string $icon_path);
     public function getUserById(int $user_id);
 }
